c6d6462ea63bc4a0d36e58087a0535a129cdddf2
leodelgadodev <leodelgado.dev@gmail.com>
2021-02-11 21:29:34 -0300

    PR fixes

--- mobile/src/app/modules/auth/auth.service.ts
+++ mobile/src/app/modules/auth/auth.service.ts
@@ -1,25 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import firebase from "firebase/app";
-import { promise } from "protractor";
-
-var fireAuth = firebase.auth();
 
 @Injectable()
 export class AuthService {
-    constructor(private http : HttpClient) {}
+    private fireAuth: firebase.auth.Auth;
+    constructor(private http : HttpClient) {
+        this.fireAuth = firebase.auth()
+    }
 
-    // public signup(password: String, email: String, name: String) : any{
-    //     return this.http.post("http://localhost:8080/auth/signup", {password, email, name}).toPromise()
-
-    // }
-
-    // public login(password: String, email: String) : any{
-    //     return this.http.post("http://localhost:8080/auth/login", {password, email}).toPromise()
-    // }
-
-    public signup(password: string, email: string, name : string) : any{
-        return fireAuth.createUserWithEmailAndPassword(email, password)
+    public signup(password: string, email: string, name : string) : Promise<string> {
+        return this.fireAuth.createUserWithEmailAndPassword(email, password)
         .then((res) => {
             res.user.updateProfile({displayName : name})
             return res.user.uid
@@ -29,34 +20,13 @@ export class AuthService {
         })
     }
 
-    public login(password: string, email: string) : any{
-        return fireAuth.signInWithEmailAndPassword(email, password)
+    public login(password: string, email: string) : Promise<string> {
+        return this.fireAuth.signInWithEmailAndPassword(email, password)
         .then((res) =>{ 
             return res.user.uid
         })
         .catch((err) => {
             throw new Error(err)
         })
-    }
-
-
-    public getHello(): any {
-        return {
-            title: this.getHelloTitle(),
-            subtitle: this.getHelloSubtitle(),
-            content: this.getHelloContent(),
-        }
-    }
-
-    private getHelloTitle(): string {
-        return 'Hello world!';
-    }
-
-    private getHelloSubtitle(): string {
-        return 'Culpa excepteur aute consequat mollit aliqua enim reprehenderit nulla.';
-    }
-
-    private getHelloContent(): string {
-        return 'Dolore dolor in culpa commodo aliqua. Ex ullamco elit labore sint amet. Velit Lorem incididunt irure ex consectetur duis magna quis. Velit enim nostrud elit sit eu nostrud nisi ad commodo nulla amet exercitation culpa voluptate. Est ea cillum dolor in veniam laboris Lorem.';
     }
 }
