30b6b8317853f6025acb8f7d35a7922235dcd665
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-03-11 19:41:56 -0300

    food view sin css

--- mobile/src/app/modules/day/food-view/food-view.component.ts
+++ mobile/src/app/modules/day/food-view/food-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { UtilsService } from "../../utils/utils.service";
+import { DayService } from "../day.service";
 import { FoodRegistry } from "../food-registry/FoodRegistry";
 
 @Component({
@@ -11,11 +13,24 @@ export class FoodViewComponent implements OnInit{
     @Input() foodType: string;
     @Input() food: FoodRegistry;
 
+    public image: string;
+    public description: string;
     
-    constructor(){}
+    constructor(private utilsService: UtilsService, private dayService: DayService){}
 
     ngOnInit(){
+        this.description = this.food.description || null
+        this.foodType = this.utilsService.capitalize(this.food.foodType)
+        if(this.food.imageId){
+            this.dayService.getImage(this.food.imageId).then(url => {
+                this.utilsService.downloadImage(url)
+                this.image = url
+            }).catch(err => console.log(err))
+        }
+    }
 
+    public content() : boolean{
+        return !this.image && !this.description
     }
 
 }
