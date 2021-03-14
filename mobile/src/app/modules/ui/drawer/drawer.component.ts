5d3124a7324ab50a9a9b21b9d5feb82052a21437
ignacio mendez <ignacio.mendez0000@gmail.com>
2021-03-13 22:20:17 -0300

    about medio pocho pero sirve por ahora jaja

--- mobile/src/app/modules/ui/drawer/drawer.component.ts
+++ mobile/src/app/modules/ui/drawer/drawer.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { Store } from '../../utils/store.service';
 
 @Component({
@@ -12,7 +13,8 @@ export class DrawerComponent {
 
   constructor(
     private router : Router,
-    private store: Store) { 
+    private store: Store,
+    private alertController: AlertController) { 
     this.username = this.store.get('username')
   }
 
@@ -22,11 +24,39 @@ export class DrawerComponent {
   }
 
   changePassword(): void {
-    console.log("change password")
+    console.log("Change password")
   }
 
-  about(): void {
-    console.log("about")
+  async about(): Promise<void> {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'About',
+      subHeader: 'version 1.0',
+      message: 'Hi!',
+      buttons: [
+        {
+          text: '@leodelgadodev',
+          handler: () => {
+            window.open("https://github.com/leodelgadodev",'_system')
+          }
+        },
+        {
+          text: '@mendezigna',
+          handler: () => {
+            window.open("https://github.com/mendezigna", '_system')
+          }
+        },
+        {
+          text: '@AntonellaDatri',
+          handler: () => {
+            window.open("https://github.com/AntonellaDatri", '_system')
+          }
+        },
+        'Ok!'
+      ]
+    });
+
+    await alert.present();
   }
 
 }
